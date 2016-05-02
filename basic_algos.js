e6a173089ae1fe7e694aefc6adf37ea2f8106c5b
Eli Byers <elibyers.dev@gmail.com>
2016-05-02 11:26:34 -0700

    findAvg, oddsArray

--- basic_algos.js
+++ basic_algos.js
@@ -42,5 +42,27 @@ function findMax(arr){
 	}
 	console.log(max);
 }
-var x = [1,3,5,7,9,13];
-findMax(x);
+// var x = [-3, 3, 5, 7];
+// findMax(x);
+
+function findAvg(arr){
+	var sum = 0;
+	for (i = 0; i < arr.length; i++){
+		sum += arr[i];
+	}
+	console.log(sum / arr.length);
+}
+// var x =  [1,3,5,7,20];
+// findAvg(x);
+
+function oddsArray(n){
+	var arr = [];
+	for (i = 0; i <= n; i++){
+		if (i % 2 !== 0){
+			arr.push(i);
+		}
+	}
+	console.log(arr);
+}
+
+oddsArray(255);
