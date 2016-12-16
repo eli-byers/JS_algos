993ee10989d875dcb1cabaa7c99f49aea3ce64cc
Eli Byers <elibyers.dev@gmail.com>
2016-12-15 21:45:10 -0800

    made memory and array, ensures good spaces

--- Python/numToText.py
+++ Python/numToText.py
@@ -7,14 +7,14 @@ def numToText(num):
 
     parts.reverse()
     count = len(parts)
-    string = ""
+    suffix = ['','','thousand','million', 'billion', 'trillion']
+    numArr = []
     for part in parts:
-        suffix = ['','','thousand ','million ', 'billion ', 'trillion ']
         if part:
-            string += "{} {}".format(hundrenToText(part), suffix[count])
+            numArr += hundrenToText(part) + [suffix[count]]
         count -= 1
 
-    return string
+    return " ".join(numArr)
 
 
 def hundrenToText(numStr):
@@ -22,24 +22,23 @@ def hundrenToText(numStr):
         numStr = "0" + str(numStr)
 
     nums = [int(x) for x in str(numStr)]
+
     numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven','eight','nine']
-    tens = ['','','twenty','thirt', 'fourty','fifty', 'sixty','seventy', 'eighty','ninety']
+    tens = ['','','twenty','thirty', 'fourty','fifty', 'sixty','seventy', 'eighty','ninety']
     teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen','seventeen','eighteen','ninteen']
 
-    string = ""
+    numArr = []
     if nums[0]:
-        string += "{} hundred".format(numbers[nums[0]])
-        string += " and " if nums[1] or nums[2] else ""
+        numArr += [numbers[nums[0]], "hundred"]
+        numArr += ["and"] if nums[1] or nums[2] else []
     if nums[1] == 1:
-        string += "{}".format( teens[nums[2]] )
+        numArr.append( teens[nums[2]] )
     else:
         if nums[1]:
-            print 1, nums[1], tens[nums[1]]
-            string += "{}".format( tens[nums[1]] )
+            numArr.append( tens[nums[1]] )
         if nums[2]:
-            print 2, nums[2], numbers[nums[2]]
-            string += "{}".format( numbers[nums[2]] )
+            numArr.append( numbers[nums[2]] )
 
-    return string
+    return numArr
 
-print numToText(101101)
+print numToText(34567893)
