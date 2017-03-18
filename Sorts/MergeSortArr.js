5c4d19f7cfccbbcc92d1c56f68ee19569a5c0e71
Eli Byers <elibyers.dev@gmail.com>
2017-03-17 17:10:51 -0700

    fix

--- Sorts/MergeSortArr.js
+++ Sorts/MergeSortArr.js
@@ -1,33 +1,30 @@
 function CombineArrs(arr1,arr2){
-	var ret = [];
-	var i = 0, k = 0;
-	while (i < arr1.length && k < arr2.length){
-		if (arr1[i] < arr2[k]){
-			ret.push(arr1[i++]);
-		} else {
-			ret.push(arr2[k++]);
-		}
-	}
-	while (i < arr1.length){
-		ret.push(arr1[i++]);
-	}
-	while (k < arr2.length){
-		ret.push(arr2[k++]);
-	}
-	return ret;
+    var ret = [];
+    var i = 0, k = 0;
+    while (i < arr1.length && k < arr2.length){
+        if (arr1[i] < arr2[k]){
+            ret.push(arr1[i++]);
+        } else {
+            ret.push(arr2[k++]);
+        }
+    }
+    while (i < arr1.length){
+        ret.push(arr1[i++]);
+    }
+    while (k < arr2.length){
+        ret.push(arr2[k++]);
+    }
+    return ret;
 }
 
 function MergeSortArr(arr){
-	if (arr.length == 1){
-		return arr;
-	}
-	var mid = Math.floor(arr.length/2);
-	var right = arr.splice(mid);
-
-	arr = MergeSortArr(arr);
-	right = MergeSortArr(right);
-
-	return CombineArrs(arr,right);
+    if (arr.length < 2) return arr;
+    var mid = Math.floor(arr.length/2);
+    var left = arr.slice(0, mid);
+    var right = arr.splice(mid);
+    left = MergeSortArr(left);
+    right = MergeSortArr(right);
+    return CombineArrs (left,right);
 }
 
 var arr = [3,6,4,7,2,1,9,11,8,5,10];
